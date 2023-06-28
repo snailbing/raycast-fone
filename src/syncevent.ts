d5aad9320b07c916f866187fac6c669bd749dcbf
nine <snailbing@gmail.com>
2023-06-28 18:02:19 +0800

    加更多的信息

--- src/syncevent.ts
+++ src/syncevent.ts
@@ -11,21 +11,32 @@ export default async function Command() {
   const startDate = new Date(curDate.getTime() - sep);
   const events = await getCalendarEvents("Fone", startDate, endDate);
   console.log(events);
-  JSON.parse(events).forEach(async (element: { summary: string; endDate: number; startDate: number; description: string; id: string; }) => {
-    // "17018", "来未来&熙牛"
-    // "16001", "HBOS"
-    const params = {
-      title: element.summary,
-      project: { value: "17018", label: "来未来&熙牛" },
-      product: { value: "16001", label: "HBOS" },
-      workHour: ((element.endDate - element.startDate) / (60 * 60 * 1000)).toFixed(1),
-      description: element.description,
-    };
-    const itemId = await createTaskAndEditWeekWork(params);
-
-    const taskUrl = "https://fone.come-future.com/fone/projectDetail/task/" + projectId + "?workItemId=" + itemId;
-    console.log(taskUrl)
-    await updateEventUrlById("Fone", taskUrl, element.id);
-  });
-  showToast(Toast.Style.Success, "Success", "Sync Calendar Event To Fone Tasks Success");
+  let success = true;
+  JSON.parse(events).forEach(
+    async (element: { summary: string; endDate: number; startDate: number; description: string; id: string }) => {
+      // "17018", "来未来&熙牛"
+      // "16001", "HBOS"
+      const params = {
+        title: element.summary,
+        project: { value: "17018", label: "来未来&熙牛" },
+        product: { value: "16001", label: "HBOS" },
+        workHour: ((element.endDate - element.startDate) / (60 * 60 * 1000)).toFixed(1),
+        description: element.description,
+      };
+      const itemId = await createTaskAndEditWeekWork(params);
+      if (itemId == null) {
+        showToast(Toast.Style.Failure, "Failure", element.summary);
+        success = false;
+        return;
+      }
+      const taskUrl = "https://fone.come-future.com/fone/projectDetail/task/" + projectId + "?workItemId=" + itemId;
+      console.log(taskUrl);
+      await updateEventUrlById("Fone", taskUrl, element.id);
+    }
+  );
+  if (success) {
+    showToast(Toast.Style.Success, "Success", "Sync Calendar Event To Fone Tasks Success");
+  } else {
+    showToast(Toast.Style.Failure, "Failure", "失败了请检查日历与FONEß");
+  }
 }
